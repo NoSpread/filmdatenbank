be7af8c77607289afd92a0a835bda63ce7c29018
Paul Ballmann <paul.ballmann@web.de>
2019-06-23 15:08:27 +0200

    control class

---
+++ src/utility/Parameters.java
@@ -0,0 +1,12 @@
+package utility;
+
+/*
+*   This interface defines the constant parameters that can be passed to our app.
+ */
+
+public interface Parameters {
+    String MOVIESEARCH = "filmsuche";
+    String ACTORSEARCH = "schauspielersuche";
+    String MOVIENETWORK = "filmnetzwerk";
+    String ACTORNETWORK = "schauspielernetzwerk";
+}
