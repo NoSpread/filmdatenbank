b19e59e73162008b8b39c1d3928d303b4a57ea4a
Paul Ballmann <paul.ballmann@web.de>
2019-06-23 13:13:52 +0200

    Parser + Reader

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
+import service.ReaderService;
+
 public class App {
-    public App() {
+    private ReaderService readerService;
+
+    public App(String[] params) {
+        this.readerService = new ReaderService();
+        System.out.println("Passed params:");
+        for (int i = 0; i < params.length; i++) {
+            System.out.println("Parameter " + i + ": " + params[i]);
+        }
         this.run();
     }
 
     private void run() {
-
+        this.readerService.readFile();
     }
 }
--- src/service/ReaderService.java
+++ src/service/ReaderService.java
@@ -1,8 +1,95 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static utility.StopCodes.*;
+
 public class ReaderService {
 
-    static void readFile() {
+    public void readFile() {
+        try {
+            File dbFile = new File("../data.db");
+            BufferedReader read = new BufferedReader(new FileReader(dbFile));
+            System.out.println("File successfully read!");
+            this.getData(read);
+        } catch (Exception e) {
+            System.out.println("File not found!");
+        }
+    }
+
+
+    private void getData(BufferedReader bufferedReader) {
+        Map<String, List<String>> dataSet = new HashMap<>();
+        String prevLine = null;
+        while (true) {
+            String data = prevLine == null ? this.readMyLine(bufferedReader) : prevLine;
+            prevLine = null;
+            if (data == null) { break; }
+            List<String> cDataList = new ArrayList<>();
+            if (hasStopCode(data)) {
+                dataSet.put(data, cDataList);
+                while (true) {
+                    String cRead = this.readMyLine(bufferedReader);
+                    if (cRead == null || hasStopCode(cRead)) {
+                        prevLine = cRead;
+                        break;
+                    }
+                    cDataList.add(cRead);
+                }
+            }
+        }
+        for (Map.Entry<String, List<String>> entry : dataSet.entrySet()) {
+            switch (entry.getKey()) {
+                case ACTOR_STOP: this.parseActors(entry.getValue()); break;
+                case MOVIE_STOP: this.parseMovies(entry.getValue()); break;
+                case DIRECTOR_STOP: this.parseDirectors(entry.getValue()); break;
+                case DIRECTOR_MOVIE_STOP: this.parseDirectorInMovie(entry.getValue()); break;
+                case ACTOR_MOVIE_STOP: this.parseActorsInMovies(entry.getValue()); break;
+                default: return;
+            }
+        }
+    }
+
+    private void parseActors(List<String> dataList) {
 
     }
+
+    private void parseMovies(List<String> dataList) {
+
+    }
+
+    private void parseDirectors(List<String> dataList) {
+
+    }
+
+    private void parseActorsInMovies(List<String> dataList) {
+
+    }
+
+    private void parseDirectorInMovie(List<String> dataList) {
+
+    }
+
+    private boolean hasStopCode(String data) {
+        if (data.contains(ACTOR_STOP) || data.contains(MOVIE_STOP) || data.contains(DIRECTOR_STOP) || data.contains(DIRECTOR_MOVIE_STOP) || data.contains(ACTOR_MOVIE_STOP)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String readMyLine(BufferedReader bufferedReader) {
+        try {
+            return bufferedReader.readLine();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
